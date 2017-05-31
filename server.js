3152a6c7ec3847f584b2ca27901ad0dae67f55c4
Steven B <genseb7@gmail.com>
2017-05-31 23:48:25 +0000

    finished microservie!

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 var express = require('express');
+var strftime = require('strftime');
 
 var port = 8080;
 var app = express();
 
-app.get('/', function(req, res) {
-  res.end('Hello World');
+function parseUnix(date) {
+   if ( isNaN(date) ) {
+    return null;
+  } 
+  return date;
+}
+
+function parseNatural(date) {
+  if ( isNaN(date) ) {
+    return null;
+  }
+  return (new Date(date)).toString();
+}
+
+function parseDate(date) {
+  var parsedDate = Date.parse(date);
+  return {
+    "unix": parseUnix(parsedDate),
+    "natural": parseNatural(parsedDate)
+  }
+}
+
+app.all('/:DATE', function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(parseDate(req.params.DATE)));
 })
 
 app.listen(port, function(err, data){
